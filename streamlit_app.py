ce30ca1a4e4bc6110f0c1511fa1a3ca82695d223
naitotekkousyo-web <naito.tekkousyo@gmail.com>
2026-02-02 14:24:01 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ components.html(
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7743829473241813"
      crossorigin="anonymous"></script>
     """,
-    height=1
+    height=50
 )
 
 import pandas as pd
@@ -237,6 +237,7 @@ if st.session_state.calc_results:
         if inst_rows:
             st.download_button("💾 CSVで保存", pd.DataFrame(inst_rows).to_csv(index=False).encode('utf-8-sig'), f"CutList_{today}.csv", "text/csv")
             st.download_button("🖨️ PDF/印刷用", pdf_html_body + "<script>window.print();</script>", f"CutList_{today}.html", "text/html")
+
 
 
 
